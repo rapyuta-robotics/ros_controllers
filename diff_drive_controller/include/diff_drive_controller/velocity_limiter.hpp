911aac2c1f437744220bb36c62ab9329ba186001
ChristofDubs <christof.dubs@gmx.ch>
2019-06-05 17:56:12 +0900

    add some explanatory comments

--- diff_drive_controller/include/diff_drive_controller/velocity_limiter.hpp
+++ diff_drive_controller/include/diff_drive_controller/velocity_limiter.hpp
@@ -4,11 +4,23 @@
  *  Created on: May 30, 2019
  *      Author: Christof Dubs
  */
-
 #pragma once
+
+#include <limits>
 
 namespace diff_drive_controller {
 
+/**
+ * \brief Class for limiting velocity commands of a differential drive type robot.
+ *
+ * Limiting the velocity command [vel_x, vel_theta] is based on a simple dynamic model.
+ * Internally, it makes use of the estimated mass to rotational inertia ratio, which is
+ * estimated from the `acc_x_max` and `acc_th_max` parameters. It is therefore important that
+ * these parameters are set to the actual limits of the robot (or that at least the ratio
+ * between the two should be identical to the ratio of the actual limits).
+ * The `vel_x_max` parameter should be set conservatively, such that simultaneous large
+ * forward and angular velocity commands can be down-scaled properly.
+ */
 class VelocityLimiter {
 public:
     struct Vector {
@@ -17,14 +29,15 @@ public:
     };
 
     struct Config {
-        float wheel_separation = 0.0f;
-        bool has_velocity_limits = false;
-        bool has_acceleration_limits = false;
-        float vel_x_max = 0.0;
-        float vel_th_max = 0.0;
-        float acc_x_max = 0.0;
-        float acc_x_min = 0.0;
-        float acc_th_max = 0.0;
+        float wheel_separation = 0.0f;         ///< [m] wheel separation distance
+        bool has_velocity_limits = false;      ///< [-] enable velocity limits
+        bool has_acceleration_limits = false;  ///< [-] enable acceleration limits
+        float vel_x_max = 0.0;                 ///< [m/s] robot's maximum achievable velocity in forward direction (>0)
+        float vel_th_max =
+                std::numeric_limits<float>::max();  ///< [rad/s] user-defined desired maximum angular velocity
+        float acc_x_max = 0.0;   ///< [m/s^2] robot's maximum achievable acceleration in forward direction (>0)
+        float acc_x_min = 0.0;   ///< [m/s^2] robot's maximum achievable deceleration in forward direction (<0)
+        float acc_th_max = 0.0;  ///< [rad/s^2] robot's maximum achievable angular acceleration (>0)
         bool isValid() const;
     };
 
